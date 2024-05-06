a3e6895c0de7913f3c0a9ec50ed97c2b8b15bc6a
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-05-06 17:14:47 -0400

    made changes to the signup button and page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const helpers = require('./utils/helpers');
 // ADD SESSION CONST //
+const session = require('express-session');
 const routes = require("./controllers")
 
 const exphbs = require('express-handlebars');
@@ -26,6 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req,res) => {
      const logged_in = true
     res.render("homepage", {logged_in})
+});
+
+app.get("/signup", (req,res) => {
+    const logged_in = true
+    res.render("signup", {logged_in})
 });
 
 app.get("/login", (req,res) => {
